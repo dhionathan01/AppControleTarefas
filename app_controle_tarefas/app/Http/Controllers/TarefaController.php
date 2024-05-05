a8661801e378418ed1cad870c7579ed7bfd6bc73
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-05-05 16:00:29 -0300

    feat: :sparkles: Auth:: Facade -> Verificando se o usuário e está ou não logado dentro dos metodos do controller

--- app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TarefaController extends Controller
 {
@@ -18,10 +19,18 @@ class TarefaController extends Controller
      */
     public function index()
     {
-        if(auth()->check()){
+       /*  if(auth()->check()){
             $id = auth()->user()->id;
             $name = auth()->user()->name;
             $email = auth()->user()->email;
+            return "Logado <br> ID: $id | Nome: $name | Email: $email";
+        }else{
+            return "Você não está logado no sistema";
+        } */
+        if(Auth::check()){
+            $id = Auth::user()->id;
+            $name = Auth::user()->name;
+            $email = Auth::user()->email;
             return "Logado <br> ID: $id | Nome: $name | Email: $email";
         }else{
             return "Você não está logado no sistema";
