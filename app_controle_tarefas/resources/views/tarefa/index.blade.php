c436cdc9847b931a6d259de50a71a2ab169515ff
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-05-15 09:17:22 -0300

    feat: :sparkles: Atualizando Tarefas

--- app_controle_tarefas/resources/views/tarefa/index.blade.php
+++ app_controle_tarefas/resources/views/tarefa/index.blade.php
@@ -14,6 +14,7 @@
                             <th scope="col">ID</th>
                             <th scope="col">Tarefa</th>
                             <th scope="col">Data limite conclusão</th>
+                            <th scope="col">Editar</th>
                           </tr>
                         </thead>
                         <tbody>
@@ -22,6 +23,7 @@
                                 <th scope="row">{{$tarefa->id}}</th>
                                 <td>{{$tarefa->tarefa}}</td>
                                 <td>{{date('d/m/Y', strtotime($tarefa->data_limite_conclusao))}}</td>
+                                <td><a href="{{route('tarefa.edit', $tarefa->id)}}">Editar</a></td>
                                 </tr>
                             @endforeach
 
