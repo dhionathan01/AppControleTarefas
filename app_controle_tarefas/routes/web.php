672d9f9fa8906d43f865acd8e515e5395c4444d8
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-05-15 09:05:27 -0300

    feat: :sparkles: Ajustando redirecionamento ao logar para view tarefas

--- app_controle_tarefas/routes/web.php
+++ app_controle_tarefas/routes/web.php
@@ -21,9 +21,9 @@ Route::get('/', function () {
 
 Auth::routes(['verify'=>true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+/* Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home')
-    ->middleware('verified');
+    ->middleware('verified'); */
 Route::get('/mensagem-teste', function(){
     return new MensagemTesteMail();
    /* Mail::to('dhionathan321@gmail.com')->send(new MensagemTesteMail());
