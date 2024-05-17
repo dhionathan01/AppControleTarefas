8ded94c48a5a8cd35aa47403b9620368349d7277
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-05-17 09:13:53 -0300

    feat: :sparkles: Definindo o tipo de papel e a orientação da impressão

--- app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -128,6 +128,10 @@ class TarefaController extends Controller
     public function exportar(){
         $tarefas = auth()->user()->tarefas()->get();
         $pdf = PDF::loadView('tarefa.pdf', ['tarefas' => $tarefas]);
+        $pdf->setPaper('a4','landscape');
+        // tipo de papel: a4 , letter
+        //orientação: landscape(paisagem) , portrait(retrato)
+        /* return $pdf->download('lista_de_tarefas.pdf'); */
         return $pdf->stream('lista_de_tarefas.pdf');
     }
 }
