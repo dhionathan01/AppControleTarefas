ff0189e177e3bcc03c8ed59cecb2e92d0f749679
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2024-05-17 09:05:42 -0300

    feat: :sparkles: Aplicando estilos ao pdf, e liberando utf-8 para caracteres

--- app_controle_tarefas/resources/views/tarefa/pdf.blade.php
+++ app_controle_tarefas/resources/views/tarefa/pdf.blade.php
@@ -1,19 +1,53 @@
-<h2>Lista de tarefas</h2>
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Tarefa</th>
-            <th>Data Limite Conclusão</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($tarefas as $key => $tarefa)
-            <tr>
-                <td>{{$tarefa->id}}</td>
-                <td>{{$tarefa->tarefa}}</td>
-                <td>{{date('d/m/Y', strtotime($tarefa->data_limite_conclusao))}}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <style>
+            .titulo{
+                border: 1px;
+                background-color: #c2c2c2;
+                text-align: center;
+                width: 100%;
+                text-transform: uppercase;
+                font-weight: bold;
+                margin-bottom: 25px;
+            }
+
+            .tabela{
+                width: 100%;
+            }
+            .page-break{
+                page-break-after: always;
+            }
+            table th {
+                text-align: left;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="titulo">Lista de tarefas</div>
+        <table class="tabela">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Tarefa</th>
+                    <th>Data Limite Conclusão</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tarefas as $key => $tarefa)
+                    <tr>
+                        <td>{{$tarefa->id}}</td>
+                        <td>{{$tarefa->tarefa}}</td>
+                        <td>{{date('d/m/Y', strtotime($tarefa->data_limite_conclusao))}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div class="page-break"></div>
+        <h2>Página 2</h2>
+        <div class="page-break"></div>
+        <h2>Página 3</h2>
+        <div class="page-break"></div>
+        <h2>Página 4</h2>
+    </body>
+</html>
